4b5fe3daff0a23434872052fe2f77fe85771a041
DialloAbdou <abdoudiallo3826@yahoo.fr>
2023-12-22 22:53:43 +0100

    Exercice Linq operation Where

---
+++ ExercicesLinq/Personne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicesLinq
+{
+    public class Personne
+    {
+        public string Nom { get; set; } = string.Empty;
+        public string Prenom { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
---
+++ ExercicesLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bogus;
+using ExercicesLinq;
+
+Console.WriteLine("Hello, World!");
+
+Randomizer.Seed = new Random(1);
+var faker = new Faker<Personne>()
+    .RuleFor(p => p.Nom, f => f.Person.LastName)
+    .RuleFor(p => p.Prenom, f => f.Person.FirstName)
+    .RuleFor(p => p.Age, f => (int)DateTime.Today.Subtract(f.Person.DateOfBirth).TotalDays / 365);
+
+var personnes = faker.Generate(100);
+
+var personnethirty = personnes.Where(p => p.Age > 30);
+
+foreach (Personne person in personnethirty)
+{
+    Console.WriteLine($" Nom: {person.Nom}, Prenom: {person.Prenom}, Age: {person.Age}");
+}
+
+Console.ReadLine();
+
+// afficher 0nom, prenom, age , des personnes qui ont plus de 30ans
